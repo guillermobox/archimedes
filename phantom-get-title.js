655c18f1f92c55742955f8cd11610a937136951e
Guillermo Indalecio Fernandez <guillermobox@gmail.com>
2016-12-11 17:48:41 +0100

    Fix get title to inhibit errors at output

--- phantom-get-title.js
+++ phantom-get-title.js
@@ -1,6 +1,8 @@
 var args = require('system').args;
 var page = require('webpage').create();
 
+page.onError = function () {};
+
 page.open(args[1], function(status) {
   console.log(page.title);
   phantom.exit();
