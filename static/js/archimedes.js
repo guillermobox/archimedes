69384aff91389c3fa16e389c6ce75edc71fc4402
Guillermo Indalecio Fernandez <guillermobox@gmail.com>
2016-12-11 17:48:22 +0100

    Testing new version of archimedes without materialcss

--- static/js/archimedes.js
+++ static/js/archimedes.js
@@ -1,63 +1,62 @@
 var archimedes = archimedes || {};
 
 function getFromPocket() {
-	var content = ich['pocket']();
-	var target = $('#ArchimedesContent')
-	$('#iframepocket', content).attr('src', 'http://localhost:8081/pocket/');
-	target.html(content);
-	$('#closepocket').click(function(ev){
-	$('#ArchimedesContent').html('');
-	});
-
-  $('#getpocket').click(function (ev){
-    var form = $('#Archimedes').contents().find('form');
-
-    form.each(function (i, element) {
-      var $inputs = $(':input', $(element));
-
-      var values = {};
-      $inputs.each(function() {
-        if ( $(this).attr('type') == 'checkbox')
-          values[this.name] = $(this).prop('checked');
-        else
-          values[this.name] = $(this).val();
-      });
-
-      if (values['Retrieve'] == "true") {
-        var resource = new archimedes.Models.Resource({
-          URL: values['URL'],
-          Folder: values['Folder'] || '/pocket',
-          Title: "",
-        });
-        resource.save({}, {
-          wait: true,
-          success: function(model, response, options) {
-            archimedes.res.add(resource);
-            /* now remove it from Pocket */
-            $.ajax("http://localhost:8081/pocket/remove/" + values["ID"]);
-            if (values['Download']) {
-              var action = new archimedes.Models.Action();
-              action.set("Target", resource.id);
-              action.set("Type", "Download");
-              action.save();
-            }
-          }
-        });
-
-      };
+  var content = ich['pocket']();
+  var target = $('#ArchimedesContent')
+  $('#iframepocket', content).attr('src', 'http://localhost:8081/pocket/');
+    target.html(content);
+    $('#closepocket').click(function(ev){
+      $('#ArchimedesContent').html('');
     });
-		$('#ArchimedesContent').html('');
-  });
 
+    $('#getpocket').click(function (ev){
+      var form = $('#Archimedes').contents().find('form');
+
+      form.each(function (i, element) {
+        var $inputs = $(':input', $(element));
+
+        var values = {};
+        $inputs.each(function() {
+          if ( $(this).attr('type') == 'checkbox')
+            values[this.name] = $(this).prop('checked');
+          else
+            values[this.name] = $(this).val();
+        });
+
+        if (values['Retrieve'] == "true") {
+          var resource = new archimedes.Models.Resource({
+            URL: values['URL'],
+            Folder: values['Folder'] || '/pocket',
+            Title: "",
+          });
+          resource.save({}, {
+            wait: true,
+            success: function(model, response, options) {
+              archimedes.res.add(resource);
+              /* now remove it from Pocket */
+              $.ajax("http://localhost:8081/pocket/remove/" + values["ID"]);
+                if (values['Download']) {
+                  var action = new archimedes.Models.Action();
+                  action.set("Target", resource.id);
+                  action.set("Type", "Download");
+                  action.save();
+                }
+            }
+          });
+
+        };
+      });
+      $('#ArchimedesContent').html('');
+    });
 }
 
-function humanFileSize(bytes, si) {
+function humanFileSize(bytes) {
   var thresh = 1024;
+  var units = ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  var u = -1;
   if (Math.abs(bytes) < thresh) {
     return bytes + ' B';
   }
-  var units = ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-  var u = -1;
   do {
     bytes /= thresh;
     ++u;
@@ -88,7 +87,7 @@ $(document).ready(function() {
         var attr = model.changedAttributes();
         var prevattr = model.previousAttributes();
         if ((attr['Downloaded'] == true && prevattr['Downloaded'] == false) ||
-          (prevattr['Downloading'] == true && attr['Downloading'] == false)) {
+        (prevattr['Downloading'] == true && attr['Downloading'] == false)) {
           var show = this.get('Title') || this.get('URL');
           Materialize.toast('<b>' + show + '</b>&nbsp;&nbsp;finished downloading', 2000);
         }
@@ -109,7 +108,7 @@ $(document).ready(function() {
     template: 'resource',
     prerender: function() {
       var d = {};
-      d['readable-disk'] = humanFileSize(this.model.attributes['DiskUsage'], true);
+      d['readable-disk'] = humanFileSize(this.model.attributes['DiskUsage']);
 
       if (this.model.attributes['Archived'] == true) {
         d['icon'] = 'close';
@@ -134,23 +133,30 @@ $(document).ready(function() {
       } else {
         d['readable-title'] = this.model.attributes['Title'];
       }
+      if (this.model.get('Kind') == 'pdf') {
+        d['content-icon'] = "fa-file-pdf-o";
+      } else if (this.model.get('Kind') == 'video') {
+        d['content-icon'] = 'fa-file-video-o';
+      } else {
+        d['content-icon'] = 'fa-file-o';
+      }
       d['readable-creation-time'] = moment(this.model.attributes['CreationTime'], 'X').fromNow();
-      d['readable-favorite-color'] = this.model.get('Favorited') == true ? 'green' : 'gray';
-      d['favorite-icon'] = this.model.get('Favorited') == true ? 'favorite' : 'favorite_border';
+      d['readable-favorite-color'] = this.model.get('Favorited') == true ? 'green' : 'black';
+      d['favorite-icon'] = this.model.get('Favorited') == true ? 'fa-heart' : 'fa-heart-o';
       return d
     },
     extraevents: {
-      'click .collapsible-header': 'toggleCollapsible',
+      'click ': 'toggleCollapsible',
       'click [data-action=OpenLink]': 'openLink',
       'click [data-action=Download]': 'download',
       'click [data-action=MarkFavorite]': 'markFavorite',
       'click [data-action=viewFull]': 'showFull',
       'click [data-action=viewImage]': 'showImage',
-      'click [data-action=ShowControls]': 'toggleControls',
+      'click [data-action=toggleControls]': 'toggleControls',
       'click [data-action=archive]': 'markArchive',
       'click [data-action=unarchive]': 'markUnarchive',
+      'click [data-action=edit]': 'edit',
       'mouseleave': 'hideControls',
-      'mouseenter': 'showControls',
     },
 
     actionfilter: {
@@ -170,22 +176,28 @@ $(document).ready(function() {
       return false;
     },
 
-    toggleControls: function(ev) {
-      $('span[data-role=controls]', this.$el).toggle();
-      $('[data-action=ShowControls]', this.$el).toggleClass('button_selected');
+    hideControls: function (ev) {
+      $('.Controls', this.$el).removeClass('Expand');
+      $('[data-action=toggleControls]', this.$el).addClass('fa-plus-square-o');
+      $('[data-action=toggleControls]', this.$el).removeClass('fa-minus-square-o');
       ev.preventDefault();
       return false;
     },
 
-    markUnarchive: function(ev) {
-      this.model.set('Archived', false);
-      this.model.save();
+    toggleControls: function(ev) {
+      $('.Controls', this.$el).toggleClass('Expand');
+      $('[data-action=toggleControls]', this.$el).toggleClass('fa-plus-square-o');
+      $('[data-action=toggleControls]', this.$el).toggleClass('fa-minus-square-o');
       ev.preventDefault();
       return false;
     },
 
     markArchive: function(ev) {
-      this.model.set('Archived', true);
+      if (this.model.get('Archived') == false) {
+        this.model.set('Archived', true);
+      } else {
+        this.model.set('Archived', false);
+      }
       this.model.save();
       ev.preventDefault();
       return false;
@@ -205,74 +217,62 @@ $(document).ready(function() {
       return false;
     },
 
-    showControls: function(ev) {
-      $('[data-role=normallyHidden]', this.$el).css('visibility', 'visible');
-    },
-
-    hideControls: function(ev) {
-      $('[data-role=normallyHidden]', this.$el).css('visibility', 'hidden');
-      $('span[data-role=controls]', this.$el).hide();
-      $('[data-action=ShowControls]', this.$el).removeClass('button_selected');
-    },
-
     showImage: function(ev) {
       if (this.model.get('Downloaded') == false || this.model.get('Kind') != 'web') {
         ev.preventDefault();
         return false;
       }
 
-      var overlay = $('#FullScreenOverlay');
+      var target = $('#ArchimedesContent')
 
       var viewer
       viewer = $('<iframe>');
       viewer.css('width', '100%');
       viewer.css('height', '100%');
-      viewer.css('margin-top', '-10px');
+      viewer.css('background-color', 'white');
       viewer.attr('src', "/screenshot/" + this.model.id + ".png");
       viewer.attr('frameborder', '0');
 
-      overlay.html(viewer);
+      target.html(viewer);
 
-      $('Archimedes').hide();
-      ev.preventDefault();
-      $('#NavigationBar').hide();
-      archimedes.nav = new archimedes.Views.ResourceNav({
-        model: this.model
-      });
-
-      Mousetrap.bind('escape', archimedes.nav.goback.bind(archimedes.nav));
-
-      archimedes.nav.render();
-      $('#NavigationBar').before(archimedes.nav.el);
-
-      overlay.show();
       return false;
     },
 
     toggleCollapsible: function(ev) {
-      if (this.model.get('Downloaded') == false) {
-        ev.preventDefault();
-        return false;
-      }
-      if (ev.ctrlKey === true) {
-        if (ev.shiftKey === true) {
-          this.showImage(ev);
+      if (this.model.get('Downloaded') != false) {
+        if (ev.ctrlKey) {
+          this.showImage(ev)
         } else {
-          this.showFull(ev);
+          this.showCollapsible(ev)
         }
-        ev.preventDefault();
-        return false;
-      }
-      if (this.$el.hasClass('active')) {
-        this.hideCollapsible(ev)
-      } else {
-        this.showCollapsible(ev)
       }
     },
 
-    hideCollapsible: function(ev) {
-      var target = $('.collapsible-body', this.$el).children();
-      target.html("");
+    showFull: function (ev) {
+      var url = this.model.get('LocalURL');
+      open(url);
+      ev.preventDefault();
+      return false;
+    },
+
+    edit: function (ev) {
+      var target = $('#ArchimedesContent');
+      var viewer = $('<div>');
+
+      viewer.append( $('<h1>').html(this.model.get('URL')));
+
+      $.each(this.model.attributes, function (key, val) {
+        var line = $('<div>');
+        var label = $('<span>').css('width', '200px').html(key);
+        var input = $('<input>').val(val);
+        line.append(label);
+        line.append(input);
+        viewer.append(line);
+      });
+      target.html(viewer);
+
+      ev.preventDefault();
+      return false;
     },
 
     showCollapsible: function(ev) {
@@ -304,55 +304,6 @@ $(document).ready(function() {
       target.html(viewer);
     },
 
-    showFull: function(ev) {
-      if (this.model.get('Downloaded') == false) {
-        ev.preventDefault();
-        return false;
-      }
-
-      var url = this.model.get('LocalURL');
-      var overlay = $('#FullScreenOverlay');
-      var viewer
-
-      if (this.model.get('Kind') == 'pdf') {
-        viewer = $('<object>');
-        viewer.css('width', '100%');
-        viewer.css('height', '100%');
-        viewer.css('margin-top', '-10px');
-        viewer.attr('data', url);
-      } else if (this.model.get('Kind') == 'video') {
-        viewer = $('<video>');
-        viewer.css('width', '100%');
-        viewer.css('height', '100%');
-        viewer.css('margin-top', '-10px');
-        viewer.attr('src', url);
-        viewer.prop('controls', true);
-        viewer.prop('autoplay', true);
-      } else {
-        viewer = $('<iframe>');
-        viewer.css('width', '100%');
-        viewer.css('height', '100%');
-        viewer.css('margin-top', '-10px');
-        viewer.attr('src', url);
-        viewer.attr('frameborder', '0');
-        viewer.attr('sandbox', 'allow-scripts allow-popups allow-same-origin allow-forms');
-      }
-
-      overlay.html(viewer);
-      overlay.show();
-
-      $('#Archimedes').hide();
-      $('#NavigationBar').hide();
-      archimedes.nav = new archimedes.Views.ResourceNav({
-        model: this.model
-      });
-      Mousetrap.bind('escape', archimedes.nav.goback.bind(archimedes.nav));
-      archimedes.nav.render();
-      $('#NavigationBar').before(archimedes.nav.el);
-
-      ev.preventDefault();
-      return false;
-    },
   });
 
   archimedes.Views.ResourceNav = archimedes.ICHView.extend({
@@ -386,7 +337,7 @@ $(document).ready(function() {
 
   archimedes.Views.ResourceList = archimedes.CollectionView.extend({
     template: 'resourcelist',
-    templateEmpty: 'noresource',
+    //templateEmpty: 'noresource',
     ModelView: archimedes.Views.ResourceRow,
   });
 
@@ -402,14 +353,48 @@ $(document).ready(function() {
   $('#Archimedes').append(archimedes.v.$el);
 
   $('[data-action=add]').click(function(ev) {
-    $('#newresource input').val('');
-    $('#URL').val('');
-    $('#Folder').val('');
-    $('#newresource').openModal({
-      in_duration: 0,
-      out_duration: 0
-    });
+    var target = $('#ArchimedesContent');
+    var content = ich['NewResource']();
+    target.html(content);
     $('#URL', '#newresource').focus();
+    $('[data-action=submit]', '#newresource').click(function() {
+      $('#addForm').submit();
+    });
+    $('#addForm').submit(function(ev) {
+      var url = $('input#URL').val();
+      var folder = $('input#Folder').val();
+      var inmediate = $('input#DownloadInmediately').prop('checked');
+      console.log(url, folder, inmediate);
+
+      if (folder.indexOf("/") !== 0) {
+        folder = "/" + folder;
+      };
+
+      var resource = new archimedes.Models.Resource({
+        URL: url,
+        Folder: folder,
+        Title: "",
+      });
+
+      resource.save(null, {
+        wait: true,
+        async: false,
+        parse: true,
+        success: function(model, response, options) {
+          archimedes.res.add(resource);
+          if (inmediate) {
+            var action = new archimedes.Models.Action();
+            action.set("Target", resource.id);
+            action.set("Type", "Download");
+            action.save();
+          }
+        }
+      });
+
+      ev.preventDefault();
+      return false;
+    });
+
   });
 
   $('[data-action=filterFavorites]').click(function(ev) {
@@ -422,9 +407,6 @@ $(document).ready(function() {
     archimedes.v.toggleArchived();
   });
 
-  $('[data-action=submit]').click(function() {
-    $('#addForm').submit();
-  });
 
   $('input#URL,input#Folder').keyup(function(event) {
     if (event.which == 13) {
@@ -432,12 +414,7 @@ $(document).ready(function() {
     }
   });
 
-  $('#FolderFilterInput').blur(function() {
-    $('#FolderFilterBox').css('opacity', '0.2');
-  });
-
   $('input#FolderFilterInput').keyup(function(event) {
-    $('#FolderFilterBox').css('opacity', '0.7');
     if (event.which == 13) {
       $('#FolderFilterInput').blur();
     } else if (event.which == 27) {
@@ -450,44 +427,7 @@ $(document).ready(function() {
     };
   });
 
-  $('#addForm').submit(function(ev) {
-    var url = $('input#URL').val();
-    var folder = $('input#Folder').val();
-    var inmediate = $('input#DownloadInmediately').prop('checked');
-
-
-    if (folder.indexOf("/") !== 0) {
-      folder = "/" + folder;
-    };
-
-    var resource = new archimedes.Models.Resource({
-      URL: url,
-      Folder: folder,
-      Title: "",
-    });
-
-    resource.save(null, {
-      wait: true,
-      async: false,
-      parse: true,
-      success: function(model, response, options) {
-        archimedes.res.add(resource);
-        $('#newresource').closeModal();
-        if (inmediate) {
-          var action = new archimedes.Models.Action();
-          action.set("Target", resource.id);
-          action.set("Type", "Download");
-          action.save();
-        }
-      }
-    });
-
-    ev.preventDefault();
-    return false;
-  });
-
   archimedes.resfresh = window.setInterval(refreshData, 1000 * 5);
-
 
   Mousetrap.bind('ctrl+v', function(e) {
     $('[data-action=add]').click();
@@ -530,6 +470,10 @@ $(document).ready(function() {
 
   Mousetrap.bind('a', function() {
     $('[data-action=filterArchived]').click();
+  });
+
+  $('#ArchimedesHead').click(function(){
+    var target = $('#ArchimedesContent').html('Hello there!');
   });
 
 });
