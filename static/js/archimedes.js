163ceef4fea3128c6d493144bfa192fd35a16ac2
Guillermo Indalecio Fernandez <guillermobox@gmail.com>
2016-12-12 09:05:43 +0100

    More react implementation, now it works

--- static/js/archimedes.js
+++ static/js/archimedes.js
@@ -1,4 +1,23 @@
 var archimedes = archimedes || {};
 
 $(document).ready(function() {
+  archimedes.Models = {};
+  archimedes.Collections = {};
+
+  archimedes.Models.Resource = Backbone.Model.extend({
+    idAttribute: "ID",
+    urlRoot: '/resources/',
+    url: function () {
+      if (this.id)
+        return this.urlRoot + this.id;
+      else
+        return this.urlRoot;
+    },
+  });
+
+  archimedes.Collections.Resources = Backbone.Collection.extend({
+    model: archimedes.Models.Resource,
+    url: "/resources/",
+  });
+
 });
