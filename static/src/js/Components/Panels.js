e8512abb78b5ea536257efe24f53cd129381b736
Guillermo Indalecio Fernandez <guillermobox@gmail.com>
2016-12-13 13:00:37 +0100

    Final touches to panel ovelay resize

--- static/src/js/Components/Panels.js
+++ static/src/js/Components/Panels.js
@@ -2,20 +2,24 @@ import React from "react";
 
 export default class Panels extends React.Component {
   startDragging (event) {
-    document.getElementById('overlaypanel').style.display = 'block';
-    document.onmouseup = this.stopDragging.bind(this);
-    document.onmousemove = this.doDrag.bind(this);
+    this.overlay = document.getElementById('overlaypanel');
+    this.overlay.style.display = 'block';
+    this.overlay.onmouseup = this.stopDragging.bind(this);
+    this.overlay.onmousemove = this.doDrag.bind(this);
+    this.overlay.onmouseleave = this.stopDragging.bind(this);
   }
   doDrag (event) {
+    console.log('drag');
     const body = document.getElementsByTagName("body")[0];
     const newRatio = 100 * (event.clientX) / (body.clientWidth);
     document.getElementById('firstpanel').style.width = 'calc(' + newRatio + "% - 4px)";
     document.getElementById('secondpanel').style.width = 'calc(' + (100 - newRatio) + "% - 4px)";
   }
   stopDragging (event) {
-    document.getElementById('overlaypanel').style.display = 'none';
-    document.onmouseup = undefined;
-    document.onmousemove = undefined;
+    this.overlay.style.display = 'none';
+    this.overlay.onmouseup = undefined;
+    this.overlay.onmousemove = undefined;
+    this.overlay.onmouseleave = undefined;
   }
   render () {
     return (
