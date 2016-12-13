a9ec0dca7f4a13a09ccf1e6a88eacc8c546ca180
Guillermo Indalecio Fernandez <guillermobox@gmail.com>
2016-12-13 15:27:21 +0100

    Add refs mechanism to panel, so no more selection through DOM

--- static/src/js/Components/Panels.js
+++ static/src/js/Components/Panels.js
@@ -3,12 +3,10 @@ import ReactDOM from "react-dom";
 
 export default class Panels extends React.Component {
   startDragging (event) {
-    this.dom = ReactDOM.findDOMNode(this)
-    this.overlay = this.dom.getElementsByClassName('Panel-Overlay')[0];
-    this.overlay.style.display = 'block';
-    this.overlay.onmouseup = this.stopDragging.bind(this);
-    this.overlay.onmousemove = this.doDrag.bind(this);
-    this.overlay.onmouseleave = this.stopDragging.bind(this);
+    this.refs.overlay.style.display = 'block';
+    this.refs.overlay.onmouseup = this.stopDragging.bind(this);
+    this.refs.overlay.onmousemove = this.doDrag.bind(this);
+    this.refs.overlay.onmouseleave = this.stopDragging.bind(this);
   }
   doDrag (event) {
     if (this.props.showDrag) {
@@ -18,23 +16,23 @@ export default class Panels extends React.Component {
   resize (x, y) {
       const body = document.getElementsByTagName("body")[0];
       const newRatio = 100 * (x) / (body.clientWidth);
-      this.dom.getElementsByClassName('Panel-First')[0].style.width = 'calc(' + newRatio + "% - 4px)";
-      this.dom.getElementsByClassName('Panel-Second')[0].style.width = 'calc(' + (100 - newRatio) + "% - 4px)";
+      this.refs.first.style.width = 'calc(' + newRatio + "% - 4px)";
+      this.refs.second.style.width = 'calc(' + (100 - newRatio) + "% - 4px)";
   }
   stopDragging (event) {
     this.resize(event.clientX, event.clientY);
-    this.overlay.style.display = 'none';
-    this.overlay.onmouseup = undefined;
-    this.overlay.onmousemove = undefined;
-    this.overlay.onmouseleave = undefined;
+    this.refs.overlay.style.display = 'none';
+    this.refs.overlay.onmouseup = undefined;
+    this.refs.overlay.onmousemove = undefined;
+    this.refs.overlay.onmouseleave = undefined;
   }
   render () {
     return (
       <div className="Panels">
-      <div className="Panel-First"> {this.props.first} </div>
-      <div className="Panel-Divisor" onMouseDown={this.startDragging.bind(this)}></div>
-      <div className="Panel-Second"> {this.props.second} </div>
-      <div className="Panel-Overlay"> </div>
+      <div ref="first" className="Panel-First"> {this.props.first} </div>
+      <div ref="divisor" className="Panel-Divisor" onMouseDown={this.startDragging.bind(this)}></div>
+      <div ref="second" className="Panel-Second"> {this.props.second} </div>
+      <div ref="overlay" className="Panel-Overlay"> </div>
       </div>
     )
   }
